1c7d76830422b031951259cc3a41d35b9c56a009
Wouter <wouter.swijgman@ixly.nl>
2022-05-11 07:00:09 +0000

    gpu accelerated calculations on batches.

--- dev/test.py
+++ dev/test.py
@@ -1,6 +1,10 @@
 import requests
+import torch
 
-model_inputs = {'prompt': 'Hello I am a [MASK] model.'}
+sample_rate = 16000
+silence = torch.zeros(1, 200*sample_rate)
+
+model_inputs = {'prompt': {'signal': silence.tolist(), 'sr': sample_rate, 'offsets':((1,2),(3,4), (5,6), (6,7), (7,8), (8,9), (9,11),(11,12),(12,14),(14,15),(15,16),(17,19),(19,20),(20,23),(23,24),(24,25))} }
 
 res = requests.post('http://localhost:8000/', json = model_inputs)
 
