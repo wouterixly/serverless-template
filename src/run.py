eb9a5c5913f8523893d81046c4c1f7b161b918d1
wouterixly <72559290+wouterixly@users.noreply.github.com>
2022-06-07 15:20:53 +0200

    Update run.py
    make process batchless for faster inferencing time and more readable code

--- src/run.py
+++ src/run.py
@@ -22,37 +22,17 @@ def run_model(model1, model2, url):
     n_clusters = len(response['training_data']['speakers'])
 
     offsets = torch.tensor(offsets)
-    durations = (offsets[:,1] * sr).to(torch.int) - (offsets[:,0] * sr).to(torch.int)
 
-    step = 16
     model1_out = torch.tensor(()).to(device)
     model2_out = []
 
-    for i in range(0,len(offsets), step):
-    
-        #change step size at last instance
-        if i >= len(offsets)-step:
-            step = len(offsets[i :])
-    
-        local_max = int(max(durations[i : i + step]))
-    
-        signals_batch = torch.tensor(())
-        signals_batch_lens = torch.tensor(())
-    
-        #pad the signals to the max of the batch and encode
-        for k in range(i , i + step):
-            signal_piece = audio[:, int( offsets[k, 0] * sr) : int( offsets[k, 1] * sr ) ]
+    for offset in offsets:
 
-            signals_batch = torch.cat((signals_batch,
-                                       torch.cat((signal_piece, torch.zeros(1, local_max - durations[k])), 1)
-                                      ), 0 )
+        signal_piece = audio[:, int( offset[0] * sr) : int( offset[1] * sr ) ].to(device)
 
-        signals_batch_lens = durations[i:i+step]/local_max
-
-        batch_cuda = signals_batch.to(device)
-        model1_out = torch.cat((model1_out, model1.encode_batch(batch_cuda, signals_batch_lens)), 0 )
-        model2_out.extend(model2.classify_batch(batch_cuda, signals_batch_lens)[3])     
-
+        model1_out = torch.cat((model1_out, model1.encode_batch(signal_piece)), 0 )
+        model2_out.extend(model2.classify_batch(signal_piece)[3])
+        
     # do postprocessing
     
 
